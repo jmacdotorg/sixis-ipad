3ceff9bf1d14ea10f02496d010a7f9817ff8110f
Jason McIntosh <jmac@jmac.org>
2012-09-14 23:49:03 -0400

    Clicking the Asmadi link takes us to Safari.

--- Sixis/SixisTemp/SixisMainMenuViewController.m
+++ Sixis/SixisTemp/SixisMainMenuViewController.m
@@ -225,4 +225,13 @@
                     }];
 }
 
+-(BOOL) webView:(UIWebView *)inWeb shouldStartLoadWithRequest:(NSURLRequest *)inRequest navigationType:(UIWebViewNavigationType)inType {
+    if ( inType == UIWebViewNavigationTypeLinkClicked ) {
+        [[UIApplication sharedApplication] openURL:[inRequest URL]];
+        return NO;
+    }
+    
+    return YES;
+}
+
 @end
